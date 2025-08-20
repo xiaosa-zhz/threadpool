38a61a714a07c495618e4e10af32427101861b8e
xiaosaOvO <dr.xiaosa@gmail.com>
2025-08-20 13:25:40 +0800

    Add stealing to illustration

--- illustrations/illustration.hpp
+++ illustrations/illustration.hpp
@@ -6,6 +6,7 @@
 #include <atomic>
 #include <memory>
 #include <thread>
+#include <array>
 #include <print>
 
 struct buffer {
@@ -15,23 +16,57 @@ struct buffer {
 
 struct buffers {
     alignas(64) std::atomic_uint64_t shared_counter = 0;
-    std::unique_ptr<buffer> bs[2];
+    std::atomic_flag steal_flag = false;
+    std::array<std::unique_ptr<buffer>, 3> bs = {
+        std::make_unique<buffer>(),
+        std::make_unique<buffer>(),
+        std::make_unique<buffer>()
+    };
 
     constexpr static std::size_t mask = ~((~0uz) >> 1);
 
-    buffer& read() {
+    buffer* read() noexcept {
+        while (steal_flag.test_and_set(std::memory_order_acquire)) {
+            std::println("wait for thief to finish");
+            std::this_thread::yield();
+        }
+        
         const auto curr = mask & shared_counter.load(std::memory_order_relaxed);
         const auto next = mask ^ curr;
         auto prev_counter = (~mask) & shared_counter.exchange(next, std::memory_order_relaxed);
-        buffer& bf = *bs[curr ? 1 : 0];
-        while (bf.local_counter.load(std::memory_order_acquire) != prev_counter) {
+        auto& buffer_handle = bs[curr ? 1 : 0];
+        while (buffer_handle->local_counter.load(std::memory_order_acquire) != prev_counter) {
             std::println("wait for local_counter to match shared_counter");
             std::this_thread::yield();
         }
-        return bf;
+        auto& local_handle = bs[2];
+        std::ranges::swap(local_handle, buffer_handle);
+
+        steal_flag.clear(std::memory_order_release);
+        return local_handle.get();
     }
 
-    bool write() {
+    buffer* steal(buffers& other) noexcept {
+        if (other.steal_flag.test_and_set(std::memory_order_acquire)) {
+            return nullptr;
+        }
+
+        const auto other_curr = mask & other.shared_counter.load(std::memory_order_relaxed);
+        const auto other_next = mask ^ other_curr;
+        auto prev_counter = (~mask) & other.shared_counter.exchange(other_next, std::memory_order_relaxed);
+        auto& buffer_handle = other.bs[other_curr ? 1 : 0];
+        while (buffer_handle->local_counter.load(std::memory_order_acquire) != prev_counter) {
+            std::println("wait for local_counter to match shared_counter in steal");
+            std::this_thread::yield();
+        }
+        auto& local_handle = bs[2];
+        std::ranges::swap(local_handle, buffer_handle);
+
+        other.steal_flag.clear(std::memory_order_release);
+        return local_handle.get();
+    }
+
+    bool write() noexcept {
         const auto curr = mask & shared_counter.fetch_add(1, std::memory_order_relaxed);
         auto& bf = *bs[curr ? 1 : 0];
 
